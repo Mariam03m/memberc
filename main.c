a149162085f7bb17aefd6d095369d7af5f3a58bf
Mariam03m <134267121+Mariam03m@users.noreply.github.com>
2025-05-06 20:41:18 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,28 +9,34 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 #include "net_client.h"
-int sockfd;
-struct sockaddr_in serv_addr;
-char buffer[BUFFER_SIZE ];
-fd_set readfds;
-int main()
-{
+#include "client_ui.h"
 
 
+int main() {
+    int sockfd = connect_to_server("127.0.0.1", 8080);
+    if (sockfd < 0) return 1;
 
-        while(1)
-        {
-            int sock = connect_to_server("105.196.107.28", 8080);
-            if (sock < 0)
-            {
-                printf("error");
-                return -1;
-            }
-            printf("hello");
+    char cmd[256], buffer[1024];
+    while (1) {
+        printf("> ");
+        fgets(cmd, sizeof(cmd), stdin);
+        cmd[strcspn(cmd, "\n")] = 0;
 
-            int soc_fd=receive_response( sock, buffer, BUFFER_SIZE);
+        if (strcmp(cmd, "quit") == 0) break;
+
+        send_command(sockfd, cmd);
+        receive_response(sockfd, buffer, sizeof(buffer));
+
+        if (strncmp(cmd, "ls", 2) == 0) {
+            display_ls(buffer);
+        } else if (strncmp(cmd, "get", 3) == 0) {
+            char *filename = strchr(cmd, ' ');
+            if (filename) download_file(sockfd, filename + 1, 0);
+        } else {
+            printf("%s\n", buffer);
         }
+    }
 
-
+    close(sockfd);
     return 0;
 }
